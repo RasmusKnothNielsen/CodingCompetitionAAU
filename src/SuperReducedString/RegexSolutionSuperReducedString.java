d1b7517e2eadf7472fc37d07266f4e251d1c0f9f
Rasmus Knoth Nielsen <Rasmus.knoth@gmail.com>
2019-12-30 16:51:17 +0100

    Added RegexSolutionSuperReducedString

---
+++ src/SuperReducedString/RegexSolutionSuperReducedString.java
@@ -0,0 +1,18 @@
+package SuperReducedString;
+
+import java.util.Scanner;
+
+public class RegexSolutionSuperReducedString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String line=scanner.nextLine();
+        while (line.matches(".*([a-z])\\1{1}.*")) {
+            line=line.replaceAll("([a-z])\\1{1}","");
+        }
+        if (line.isEmpty()) {
+            System.out.println("Empty String");
+        } else {
+            System.out.println(line);
+        }
+    }
+}

5dd12f57eb0eadd55ad5e33e7284e15c295f1823
Rasmus Knoth Nielsen <Rasmus.knoth@gmail.com>
2020-03-23 10:10:04 +0100

    Added MissingNumbers

--- Python/MissingNumbers.py
+++ Python/MissingNumbers.py
@@ -1,35 +1,61 @@
-def convert_to_int_array(string):
+"""
+Missing Numbers.
+Numeros the Artist had two lists that were permutations of one another. He was very proud. Unfortunately, while transporting them from one exhibition to another, some numbers were lost
+out of the first list. Can you find the missing numbers?
+
+Find the PDF for full description in Exercises.
+
+@Author: Rasmus Knoth Nielsen, 2020 Advanced Python
+"""
+
+from collections import Counter
+
+
+def get_user_input():
+    # Getting the inputs from user
+    size_of_first_array = input("Size of first sample: ")
+    first_array_string = input("Integers in first array, separated by space: ")
+    size_of_second_array = input("Size of second sample: ")
+    second_array_string = input("Integers in second array, separated by space: ")
+
+    # Converting strings to arrays
+    arr = convert_string_to_array(first_array_string)
+    brr = convert_string_to_array(second_array_string)
+
+    return arr, brr
+
+
+def convert_string_to_array(string):
     string_array = string.split(" ")
-    int_array = []
-    for str in string_array:
-        int_array.append(int(str))
-    return int_array
+    return string_array
+
+
+def compare_length(arr, brr):
+    shortest, longest = [], []
+    if len(arr) > len(brr):
+        longest = arr
+        shortest = brr
+    else:
+        longest = brr
+        shortest = arr
+
+    return shortest, longest
+
+
+def get_difference(arr, brr):
+    c1 = Counter(brr)
+    c2 = Counter(arr)
+
+    return c1 - c2
 
 
 if __name__ == "__main__":
-
-    # Getting the inputs from user
-    #size_first_array = int(input("Size of first input?"))
-    first_array_string = input("Integers in first array, separated by space:")
-    #size_second_array = int(input("Size of second input?"))
-    second_array_string = input("Integers in second array, separated by space:")
-
-    # Convert string input into int array
-    first_array = convert_to_int_array(first_array_string)
-    second_array = convert_to_int_array(second_array_string)
-
-    # Figure out which array is the longest, and use that as a benchmark
-    long_array = []
-    short_array = []
-    if first_array.count() > second_array.count():
-        long_array = first_array
-        short_array = second_array
-    else:
-        long_array = second_array
-        short_array = first_array
-
-    missing_numbers = []
-    #for number in short_array:
-
-
-    print(first_array)
+    # Get the user input
+    first_array, second_array = get_user_input()
+    # Find the longest and shortest array
+    short_array, long_array = compare_length(first_array, second_array)
+    # Get the difference
+    diff = get_difference(short_array, long_array)
+    # Sort the list and print it out as a single string
+    diff = sorted(list(diff.elements()))
+    print(' '.join(diff))
